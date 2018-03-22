04284b6a08c706248a39c31cf1d08e867f0a1e20
Tyler Parsons <tparsons17@bloomberg.net>
2018-03-22 15:58:40 -0400

    Implement context controlled form

--- subjects/08 Context/exercise.js
+++ subjects/08 Context/exercise.js
@@ -20,32 +20,101 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 class Form extends React.Component {
+  values = {};
+  resetListeners = [];
+
+  static childContextTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onReset: PropTypes.func.isRequired,
+    addResetListener: PropTypes.func.isRequired,
+  };
+
+  getChildContext() {
+    return {
+      onSubmit: () => this.props.onSubmit(this.values),
+      onChange: (key, value) => this.values[key] = value,
+      onReset: () => this.resetListeners.forEach(l => l()),
+      addResetListener: (listener) => this.resetListeners.push(listener)
+    };
+  }
+
   render() {
     return <div>{this.props.children}</div>;
   }
 }
 
 class SubmitButton extends React.Component {
+  static contextTypes = {
+    onSubmit: PropTypes.func
+  };
+
   render() {
-    return <button>{this.props.children}</button>;
+    return <button onClick={this.context.onSubmit}>
+      {this.props.children}
+    </button>;
+  }
+}
+
+class ResetButton extends React.Component {
+  static contextTypes = {
+    onReset: PropTypes.func
+  };
+
+  render() {
+    return <button onClick={this.context.onReset}>
+      {this.props.children}
+    </button>;
   }
 }
 
 class TextInput extends React.Component {
+  state = {
+    value: ""
+  };
+
+  static contextTypes = {
+    onSubmit: PropTypes.func,
+    onChange: PropTypes.func,
+    addResetListener: PropTypes.func
+  };
+
+  handleKeyPress = (evt) => {
+    if (evt.key === "Enter") {
+      this.context.onSubmit && this.context.onSubmit();
+    }
+  };
+
+  updateValue(value) {
+    this.context.onChange(this.props.name, value);
+    this.setState({value});
+  }
+
+  handleChange = (evt) => {
+    this.updateValue(evt.target.value);
+  };
+
+  componentDidMount() {
+    this.context.addResetListener(() => this.updateValue(""));
+  }
+
   render() {
     return (
       <input
         type="text"
         name={this.props.name}
+        value={this.state.value}
         placeholder={this.props.placeholder}
+        onKeyPress={this.handleKeyPress}
+        onChange={this.handleChange}
       />
     );
   }
 }
 
 class App extends React.Component {
-  handleSubmit = () => {
-    alert("YOU WIN!");
+  handleSubmit = (formData) => {
+    alert("YOU WIN! Form is " + JSON.stringify(formData));
   };
 
   render() {
@@ -62,6 +131,9 @@ class App extends React.Component {
           </p>
           <p>
             <SubmitButton>Submit</SubmitButton>
+          </p>
+          <p>
+            <ResetButton>Reset</ResetButton>
           </p>
         </Form>
       </div>
